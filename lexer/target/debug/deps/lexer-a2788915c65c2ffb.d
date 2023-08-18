b6a16bd6649b093b4844f7085e3861270f29f821
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-08-19 01:12:55 +0300

    create lexer logic for lexer

---
+++ lexer/target/debug/deps/lexer-a2788915c65c2ffb.d
@@ -0,0 +1,5 @@
+/home/hp/Desktop/cairovm/lexer/target/debug/deps/lexer-a2788915c65c2ffb: src/main.rs
+
+/home/hp/Desktop/cairovm/lexer/target/debug/deps/lexer-a2788915c65c2ffb.d: src/main.rs
+
+src/main.rs:
---
+++ lexer/target/debug/lexer.d
@@ -0,0 +1,1 @@
+/home/hp/Desktop/cairovm/lexer/target/debug/lexer: /home/hp/Desktop/cairovm/lexer/src/main.rs
