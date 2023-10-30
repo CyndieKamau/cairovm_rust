97ce5598ba2c4ab0668c6869f71d9db6a94060ca
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-10-31 02:50:29 +0300

    Import logos, create enum 'Token' using logos

--- src/lexer/logos_lexer.rs
+++ src/lexer/logos_lexer.rs
@@ -1,3 +1,128 @@
+use logos::Logos;
+
+#[derive(Debug, Logos, PartialEq)]
+#[logos(skip r"[ \t\n\f]+")] 
+
+enum Token {
+
+    #[token("as")]
+    As,
+
+    #[token("break")]
+    Break,
+
+    #[token("const")]
+    Const,
+
+    #[token("continue")]
+    Continue,
+
+    #[token("else")]
+    Else,
+
+    #[token("enum")]
+    Enum,
+
+    #[token("false")]
+    False,
+
+    #[token("fn")]
+    Fn,
+
+    #[token("if")]
+    If,
+
+    #[token("for")]
+    For,
+
+    #[token("let")]
+    Let,
+
+    #[token("loop")]
+    Loop,
+
+    #[token("match")]
+    Match,
+
+    #[token("mut")]
+    Mut,
+
+    #[token("of")]
+    Of,
+
+    #[token("return")]
+    Return,
+
+    #[token("struct")]
+    Struct,
+
+    #[token("true")]
+    True,
+
+    #[token("trait")]
+    Trait,
+
+    #[token("fieldElement")]
+    Felt252,
+
+    #[token(".")]
+    Period,
+
+    #[token("!")]
+    Exclamation,
+
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("=")]
+    Assign,
+
+    #[token("==")]
+    Equal,
+
+    #[token(";")]
+    SemiColon,
+
+    #[token("*")]
+    Asterik,
+
+    #[token(":")]
+    Colon,
+
+    #[token("_")]
+    UnderScore,
+
+    #[token("/")]
+    Divide,
+
+    #[token("%")]
+    Percent,
+
+    #[token("&")]
+    Ampersand,
+
+    #[token("->")]
+    Arrow,
+
+    #[token("()")]
+    Parenthesis,
+
+    #[token("{}")]
+    Braces,
+
+    #[token("[]")]
+    Bracket,
+
+    
+
+
+
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
