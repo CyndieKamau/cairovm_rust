62ba7f6e792bee4b5267ddc08e041a8429ffc721
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-11-29 10:56:43 +0300

    Create Backend API with Actix Web Server

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,64 @@
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use std::env;
+
 use logos::Lexer;
 use crate::lexer::Token;
 mod lexer;
 
 
-fn main() {
-    // Replace this with the actual source code you want to tokenize
-    let input = "let mut x: u32 = 5;"; 
+#[derive(Deserialize)]
+struct CodeInput {
+    code: String,
+}
 
-    let mut lexer = Lexer::<Token>::new(input);
+#[derive(Serialize)]
+struct TokenInfo {
+    token_type: String,   // Representing the type of the token (e.g., "Let", "Identifier", etc.)
+    span: (usize, usize), // Tuple representing the start and end positions
+    slice: String,        // The slice of the source code corresponding to this token
+}
+
+#[derive(Serialize)]
+struct TokenOutput {
+    tokens: Vec<TokenInfo>,
+}
+
+async fn tokenize_code(input: web::Json<CodeInput>) -> impl Responder {
+    let mut lexer = Lexer::<Token>::new(&input.code);
+    let mut tokens = Vec::new();
 
     while let Some(result) = lexer.next() {
         match result {
             Ok(token) => {
-                // Process the token, for example, print it
-                println!("{:?} (Span: {:?}, Slice: '{}')", token, lexer.span(), lexer.slice());
+                let token_info = TokenInfo {
+                    token_type: format!("{:?}", token), // Adjust based on how you want to represent the token type
+                    span: (lexer.span().start, lexer.span().end),
+                    slice: lexer.slice().to_string(),
+                };
+                tokens.push(token_info);
             },
             Err(_) => {
                 // Handle lexical errors here
+                // For now, we'll just log to console and continue
                 eprintln!("Lexical error at span: {:?}", lexer.span());
             }
         }
     }
+
+    HttpResponse::Ok().json(TokenOutput { tokens })
+}
+
+//Setting up the Actix web server
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();  //Initialize the logger
+
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/tokenize").route(web::post().to(tokenize_code)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
